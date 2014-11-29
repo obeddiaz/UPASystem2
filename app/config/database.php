6570174307c76d36e37d8238bb54cf9feb377cbe
Raziel Lupercio <raziel.lupercio@hotmail.com>
2014-11-28 18:06:43 -0600

    Descuentos Controller

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => '127.0.0.1',
 			'database'  => 'pagos',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'root',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
--- app/models/Descuentos.php
+++ app/models/Descuentos.php
@@ -1,7 +1,7 @@
 <?php
 
 class  Descuentos extends \Eloquent {
-	protected $fillable = ['id','tipos_importe_id','adeudos_id', 'importe', 'created_at', 'updated_at'];
+	protected $fillable = ['id','tipo_importe_id','adeudos_id', 'importe', 'created_at', 'updated_at'];
 	protected $table = 'descuentos';
 	public $timestamps = true;
 }
