3875f90af3648fc861489312768b785084580662
Obed Díaz <obed.diaz.r@gmail.com>
2014-12-02 22:05:37 -0600

    Modificar Adeudos

--- app/models/Adeudos.php
+++ app/models/Adeudos.php
@@ -46,14 +46,14 @@ class Adeudos extends \Eloquent {
         $query = Adeudos::join('sub_conceptos as sc', 'sc.id', '=', 'adeudos.sub_concepto_id')
                 ->where("adeudos.id_persona", "=", $data['id_persona'])
                 ->where("adeudos.periodo", "=", $data['periodo'])
-                ->select('adeudos.*', DB::raw("period_diff(date_format(now(), '%Y%m'), date_format(`fecha_limite`, '%Y%m')) as meses_retraso"), 'sc.aplica_beca')
+                ->select('adeudos.*', DB::raw("period_diff(date_format(now(), '%Y%m'), date_format(`fecha_limite`, '%Y%m')) as meses_retraso"), 'sc.aplica_beca', 'sc.sub_concepto')
                 ->get();
         $now = strtotime('now');
         $daynow = date('d', $now);
         $tiene_beca = Becas::AlumnoBeca_Persona_Periodo($data);
         $sub_cont = array();
         foreach ($query as $key => $adeudo) {
-
+            $query[$key]['importe_inicial'] = $query[$key]['importe'];
             if (isset($sub_cont[$adeudo['sub_concepto_id']])) {
                 $sub_cont[$adeudo['sub_concepto_id']]+=1;
             } else {
@@ -72,6 +72,9 @@ class Adeudos extends \Eloquent {
                 if ($daynow > $day) {
                     $query[$key]['meses_retraso'] = $adeudo['meses_retraso'] + 1;
                 }
+                if ($query[$key]['meses_retraso'] <= 0) {
+                    $query[$key]['recargo_total'] = 0;
+                }
                 if ($query[$key]['meses_retraso'] > 0) {
                     if ($tiene_beca) {
                         $databeca = array(
@@ -84,6 +87,7 @@ class Adeudos extends \Eloquent {
                     }
                     $recargo = $commond->calcular_importe_por_tipo($adeudo['importe'], $adeudo['recargo'], $adeudo['tipo_recargo']);
                     $recargo*= $query[$key]['meses_retraso'];
+                    $query[$key]['recargo_total'] = $recargo;
                     $query[$key]['importe']+=$recargo;
                 } elseif ($tiene_beca && ($adeudo['aplica_beca'] == 1)) {
                     $beca = $commond->calcular_importe_por_tipo($adeudo['importe'], $tiene_beca['importe'], $tiene_beca['tipo_importe_id']);
@@ -94,21 +98,27 @@ class Adeudos extends \Eloquent {
                 $fecha_pago = date('d', $pago);
                 $fecha_limite = strtotime($adeudo['fecha_limite']);
                 $day = date('d', $fecha_limite);
-                if ($tiene_desceunto) {
-                    $descuento = $commond->calcular_importe_por_tipo($adeudo['importe'], $tiene_desceunto['importe'], $tiene_desceunto['tipo_importe_id']);
+                foreach ($tiene_desceunto as $descuentodata) {
+                    $descuento = $commond->calcular_importe_por_tipo($adeudo['importe'], $descuentodata['importe'], $descuentodata['tipo_importe_id']);
                     $query[$key]['tiene_desceunto'] = 1;
                     $query[$key]['importe']-=$descuento;
                 }
                 if ($fecha_pago > $day) {
                     $query[$key]['meses_retraso'] = $adeudo['meses_retraso'] + 1;
                 }
+                if ($query[$key]['meses_retraso'] <= 0) {
+                    $query[$key]['recargo_total'] = 0;
+                }
+                
                 if ($query[$key]['meses_retraso'] > 0) {
                     $recargo = $commond->calcular_importe_por_tipo($adeudo['importe'], $adeudo['recargo'], $adeudo['tipo_recargo']);
                     $recargo*= $query[$key]['meses_retraso'];
+                    $query[$key]['recargo_total'] = $recargo;
                     $query[$key]['importe']+=$recargo;
                 } elseif ($tiene_beca && ($adeudo['aplica_beca'] == 1)) {
                     $beca = $commond->calcular_importe_por_tipo($adeudo['importe'], $tiene_beca['importe'], $tiene_beca['tipo_importe_id']);
                     $query[$key]['importe']-=$beca;
+                    
                 }
             }
         }
