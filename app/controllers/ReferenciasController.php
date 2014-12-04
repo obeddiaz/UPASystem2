87c549d94b8efca94fc564bb1f648d2cc99f0634
Obed Díaz <obed.diaz.r@gmail.com>
2014-12-03 19:43:09 -0600

    Errores Referencias

--- app/controllers/ReferenciasController.php
+++ app/controllers/ReferenciasController.php
@@ -19,59 +19,58 @@ class ReferenciasController extends \BaseController {
      */
     public function create() {
         $commond = new Common_functions();
-        $parametros['adeudos']=Adeudos::obtener_adeudos_alumno(array('id_persona'=>838,'periodo'=>143));
-         
-        $reglas = array( 
-            'adeudos'  => 'required'
-        );
-        $validator = Validator::make($parametros,$reglas);
+        $parametros['adeudos'] = Input::get(); //Adeudos::obtener_adeudos_alumno(array('id_persona'=>838,'periodo'=>143));
 
-        if (!$validator->fails())
-        {
+        $reglas = array(
+            'adeudos' => 'required'
+        );
+        $validator = Validator::make($parametros, $reglas);
+
+        if (!$validator->fails()) {
             $libereriaReferencia = new Referencias();
-            $data=array();
-            $data['importe_total']=0;
+            $data = array();
+            $data['importe_total'] = 0;
             foreach ($parametros['adeudos'] as $key => $value) {
-                if ($value['status_adeudo']==0) {    
+                if ($value['status_adeudo'] == 0) {
                     foreach ($parametros['adeudos'] as $key_a => $value_a) {
                         if (!isset($fecha_limite)) {
-                            if ($value_a['fecha_limite']>date('Y-m-d')) {
-                                $fecha_limite=$value_a['fecha_limite'];
+                            if ($value_a['fecha_limite'] > date('Y-m-d')) {
+                                $fecha_limite = $value_a['fecha_limite'];
                             } else {
-                                if ($parametros['adeudos'][count($parametros['adeudos'])-1]['fecha_limite']==$value_a['fecha_limite'] && !isset($fecha_limite)) {
-                                    $fecha=$value_a['fecha_limite'];
-                                    $months= $value_a['meses_retraso'];
-                                    $fecha_limite=date("Y-m-d", strtotime("$fecha +$months month"));           
+                                if ($parametros['adeudos'][count($parametros['adeudos']) - 1]['fecha_limite'] == $value_a['fecha_limite'] && !isset($fecha_limite)) {
+                                    $fecha = $value_a['fecha_limite'];
+                                    $months = $value_a['meses_retraso'];
+                                    $fecha_limite = date("Y-m-d", strtotime("$fecha +$months month"));
                                 }
                             }
                         }
                     }
-                    $subconcepto=Sub_conceptos::find($value['sub_concepto_id']);
-                    $referencia=sprintf('%05d',$value['id_persona']).
-                                sprintf('%03d',$value['periodo']).
-                                sprintf('%03d',$value['sub_concepto_id']).
-                                sprintf('%01d',$value['contador']);
-                    $data['referencias'][$key]['referencia']=$libereriaReferencia->Generar($referencia,$value['importe'],$fecha_limite);
-                    $data['referencias'][$key]['importe']=json_decode($value['importe']);
-                    $data['referencias'][$key]['sub_concepto']=$subconcepto['sub_concepto'];
+                    $subconcepto = Sub_conceptos::find($value['sub_concepto_id']);
+                    $referencia = sprintf('%05d', $value['id_persona']) .
+                            sprintf('%03d', $value['periodo']) .
+                            sprintf('%03d', $value['sub_concepto_id']) .
+                            sprintf('%01d', $value['contador']);
+                    $data['referencias'][$key]['referencia'] = $libereriaReferencia->Generar($referencia, $value['importe'], $fecha_limite);
+                    $data['referencias'][$key]['importe'] = json_decode($value['importe']);
+                    $data['referencias'][$key]['sub_concepto'] = $subconcepto['sub_concepto'];
                     $data['importe_total']+=$value['importe'];
-                    $data['fecha_limite']=$fecha_limite;
-                    $data['periodo']=$value['periodo'];
-                    $data['persona']=$commond->obtener_infoAlumno_idPersona(array('id_persona'=>$value['id_persona']));
-                    $existe_referencia=Referencia::where('referencia','=',$data['referencias'][$key]['referencia'])->first();
-                    $cuentas=Cuentas::where('activo_cobros','=',1)->first();
-                    $data['convenio']=$cuentas['cuenta'];
+                    $data['fecha_limite'] = $fecha_limite;
+                    $data['periodo'] = $value['periodo'];
+                    $data['persona'] = $commond->obtener_infoAlumno_idPersona(array('id_persona' => $value['id_persona']));
+                    $existe_referencia = Referencia::where('referencia', '=', $data['referencias'][$key]['referencia'])->first();
+                    $cuentas = Cuentas::where('activo_cobros', '=', 1)->first();
+                    $data['convenio'] = $cuentas['cuenta'];
                     if (!$existe_referencia) {
-                         Referencia::create(
-                            array(
-                                'referencia'=>$data['referencias'][$key]['referencia'],
-                                'adeudos_id'=>$value['id'],
-                                'cuentas_id'=>$cuentas['id']
-                            ));
+                        Referencia::create(
+                                array(
+                                    'referencia' => $data['referencias'][$key]['referencia'],
+                                    'adeudos_id' => $value['id'],
+                                    'cuentas_id' => $cuentas['id']
+                        ));
                     }
                 }
             }
-            $res['data']=$data;
+            $res['data'] = $data;
             echo json_encode(array('error' => false, 'mensaje' => 'Nuevo registro', 'respuesta' => $res));
         } else {
             echo json_encode(array('error' => true, 'mensaje' => 'No hay parametros o estan mal.', 'respuesta' => null));
@@ -149,8 +148,8 @@ class ReferenciasController extends \BaseController {
         $validator = Validator::make($parametros, $reglas);
         if (!$validator->fails()) {
             foreach ($parametros as $key => $value) {
-                if (!array_key_exists($key,$reglas)) {
-                    unset($parametros[$key]);   
+                if (!array_key_exists($key, $reglas)) {
+                    unset($parametros[$key]);
                 }
             }
             $res = Referencia::where('id', '=', $parametros['id'])->update($parametros);
@@ -169,7 +168,7 @@ class ReferenciasController extends \BaseController {
     public function destroy() {
         $parametros = Input::get();
         $reglas = array(
-                    'id' => 'required|integer',
+            'id' => 'required|integer',
         );
         $validator = Validator::make($parametros, $reglas);
         if (!$validator->fails()) {
@@ -186,8 +185,8 @@ class ReferenciasController extends \BaseController {
         $file = Input::file('referencia_archivo');
         if (isset($file)) {
             $data_file = Archivo_referencias::leer($file);
-            $data_file['infoFile']['nombre_archivo']=Input::file('referencia_archivo')->getClientOriginalName();
-            $infoarchivo = array(    
+            $data_file['infoFile']['nombre_archivo'] = Input::file('referencia_archivo')->getClientOriginalName();
+            $infoarchivo = array(
                 'no_transacciones' => $data_file['infoFile']['TRANSACCIONES'],
                 'cobro_inmediato' => $data_file['infoFile']['COBROINMEDIATO'],
                 'comisiones_creadas' => $data_file['infoFile']['COMISIONESCOBRADAS'],
@@ -198,23 +197,23 @@ class ReferenciasController extends \BaseController {
                 'nombre_archivo' => $data_file['infoFile']['nombre_archivo'],
                 'fecha' => date('Y-m-d')
             );
-            $personas=array();
-            $i=0;
+            $personas = array();
+            $i = 0;
             foreach ($data_file['referencias'] as $key => $value) {
                 $adeudo = Referencia::with('adeudos')
-                        ->where('referencia','=',$value['referencia'])->first(); 
-                if ($value['importe']>=$adeudo['adeudos']['importe']) {
-                    Adeudos::where('id','=',$adeudo['adeudos']['id'])->update(array('status_adeudo' => 1, 'fecha_pago' => date('Y-m-d')));
-                } else{
-                    Adeudos::where('id','=',$adeudo['adeudos']['id'])->update(array('importe' => floatval($adeudo['impote'] - floatval($value['importe'])), 'fecha_pago' => date('Y-m-d')));
+                                ->where('referencia', '=', $value['referencia'])->first();
+                if ($value['importe'] >= $adeudo['adeudos']['importe']) {
+                    Adeudos::where('id', '=', $adeudo['adeudos']['id'])->update(array('status_adeudo' => 1, 'fecha_pago' => date('Y-m-d')));
+                } else {
+                    Adeudos::where('id', '=', $adeudo['adeudos']['id'])->update(array('importe' => floatval($adeudo['impote'] - floatval($value['importe'])), 'fecha_pago' => date('Y-m-d')));
                 }
 
-                $personas[$i]['persona']=$commond->obtener_infoAlumno_idPersona(array('id_persona'=>$adeudo['adeudos']['id_persona']));
+                $personas[$i]['persona'] = $commond->obtener_infoAlumno_idPersona(array('id_persona' => $adeudo['adeudos']['id_persona']));
                 $personas[$i]['referencia'] = $value['referencia'];
-            
+
                 $i++;
             }
-            $res['data']=$personas;
+            $res['data'] = $personas;
             Respuesta_bancaria::create($infoarchivo);
             return json_encode(array('error' => false, 'mensaje' => '', 'respuesta' => $res));
         } else {
--- app/libraries/common_function.php
+++ app/libraries/common_function.php
@@ -44,7 +44,7 @@ class Common_functions {
 
         if (is_array($persona)) {
             foreach ($alumnos as $key_alumnos => $alumno) {
-                if ($alumno['id'] == intval($persona['id_persona'])) {
+                if ($alumno['idpersonas'] == intval($persona['id_persona'])) {
                     $persona_info = $persona;
                     unset($persona_info['id_persona']);
                     $res[] = array_merge($alumno, $persona_info);
