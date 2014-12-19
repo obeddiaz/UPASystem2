d999fcec4d33390d92c26dd6a78c9f4ab0cf13c9
diegomjasso <maciasjasso0@hotmail.com>
2014-12-18 18:26:23 -0600

    error de grado

--- app/models/Adeudos_tipopago.php
+++ app/models/Adeudos_tipopago.php
@@ -2,7 +2,7 @@
 
 class Adeudos_tipopago extends \Eloquent {
 	protected $fillable = [ 'adeudos_id', 'created_at', 'id', 'tipo_pago_id', 'updated_at'];
-	protected $table = 'banco';
+	protected $table = 'adeudos_tipopago';
 	public $timestamps = true;
 }
 
