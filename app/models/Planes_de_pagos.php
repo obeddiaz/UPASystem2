2f9ec1e9b6af2c4eba56b6e27d5ad413baebda5f
diegomjasso <maciasjasso0@hotmail.com>
2015-03-24 00:22:12 -0600

    cambios

--- app/models/Planes_de_pagos.php
+++ app/models/Planes_de_pagos.php
@@ -38,7 +38,8 @@ class Planes_de_pago extends \Eloquent {
                         'subconcepto_paqueteplandepago.fecha_de_vencimiento', 
                         'subconcepto_paqueteplandepago.recargo', 
                         'subconcepto_paqueteplandepago.tipo_recargo',
-                        'subconcepto_paqueteplandepago.tipos_pago'
+                        'subconcepto_paqueteplandepago.tipos_pago',
+                        'subconcepto_paqueteplandepago.recargo_acumulado'
                         )
                 ->orderBy('subconcepto_paqueteplandepago.id', 'ASC')
                 ->get();
