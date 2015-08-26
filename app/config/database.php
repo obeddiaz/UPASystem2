ed981e9db7ba211bcb1ebaeb2d07f508984d181b
gis <diego.macias@STKAG17783.softtekge.com>
2015-08-26 17:10:46 -0500

    changes

--- app/config/database.php
+++ app/config/database.php
@@ -54,7 +54,7 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost:8088/',
+			'host'      => '127.0.0.1',
 			'database'  => 'pagos',
 			'username'  => 'root',
 			'password'  => '',
